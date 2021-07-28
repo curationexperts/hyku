92ab6dcd1c08404d3b7d1c4412199c536bfa9c8e
McClain Looney <m@loonsoft.com>
2021-07-28 13:11:56 -0500

    default db host, updated .gitignore, link .env

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/opt/hyku"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/master.key"
+append :linked_files, "config/master.key", ".env.#{fetch :stage}"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
